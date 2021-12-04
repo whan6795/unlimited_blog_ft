125ceecee34e8a75d4a4225877825a97d63d98ae
dawangba <1808003841@qq.com>
2021-12-04 18:18:38 +0800

    2021/12/4

---
+++ src/http.ts
@@ -0,0 +1,40 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+// loading动画
+import { ElLoading } from 'element-plus'
+
+let loading: any
+
+const startLoading = () => {
+  interface Options {
+    lock: boolean,
+    text: string,
+    background: string
+  }
+  const options: Options = {
+    lock: true,
+    text: '加载中',
+    background: 'rgba(0,0,0,0.7)'
+  }
+  loading = ElLoading.service(options)
+}
+
+const endLoading = () => {
+  loading.close()
+}
+
+// 请求拦截
+axios.interceptors.request.use((config: AxiosRequestConfig) => {
+  startLoading()
+  return config
+})
+
+//相应拦截
+axios.interceptors.response.use((response: AxiosResponse<any>) => {
+  endLoading()
+  return response
+}, error => {
+  // 错误提醒
+  endLoading()
+  return Promise.reject(error)
+})
+export default axios
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import axios from './http'
 
-createApp(App).use(store).use(router).mount('#app')
+// 全局挂载axios
+createApp(App).config.globalProperties.$axios = axios
+createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
